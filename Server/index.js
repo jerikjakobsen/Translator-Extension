5b664da75527d15c90bb7ae0dbd9780bffe150d3
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2023-05-28 14:41:07 -0400

    Update server to use express for routing, and include available languages route

--- Server/index.js
+++ Server/index.js
@@ -1,21 +1,37 @@
 const http = require('http');
-const TranslatorSocket = require("./TranslatorSocket.js")
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello, world!');
-    res.end();
+const express = require('express');
+const app = express();
+const TranslatorSocket = require("./TranslatorSocket.js");
+const languages = require("./languages.json");
+app.get('/availableLanguages', (req, res) => {
+  const namesToCodes = Object.entries(languages.translation).map(([key, val]) => {
+    var nameToCode = {};
+    nameToCode[val.name] = key;
+    return nameToCode;
   });
+  res.status(200).json(namesToCodes);
+})
+
+app.get("/", (req, res) => {
+  res.send("Translator Extension Server");
+})
+
+const server = http.Server(app);
+server.listen(3000);
+
 const io = require('socket.io')(server, {cors: {
   origin: "*",
   methods: ["GET", "POST"]
 }});
 
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
-
+const connectedClients = {};
 io.on('connection', (socket) => {
+  connectedClients[socket.id] = socket
+  console.log("CONNECTION!", Object.values(connectedClients).length)
   const translatorSocket = new TranslatorSocket(socket);
-
+  socket.on('disconnect', () => {
+    // Remove the disconnected socket from the object
+    delete connectedClients[socket.id];
+    console.log(socket.id, Object.values(connectedClients).length)
+  });
 });
